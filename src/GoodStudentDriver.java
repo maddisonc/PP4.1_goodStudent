b6a67381851a670ffac53847b76ee5e1e7785e29
Maddison C <38960421+Maddifish@users.noreply.github.com>
2021-10-15 08:14:18 -0700

    Update GoodStudentDriver.java

--- src/GoodStudentDriver.java
+++ src/GoodStudentDriver.java
@@ -10,14 +10,32 @@ public class GoodStudentDriver
 
 
 //        GoodStudent gates = new GoodStudent("Bill", "Gates", 3.98, true);
-//
 //        System.out.println(hopper.CreateID());
 //        System.out.println(gates.CSGPA());
 
+        // Call toString to output Alan Turing
         System.out.println(turing.toString());
-        System.out.println();
+
+        // Call setters & getters to update and output Florence’s ID: 95366 and gpa: 3.92
+        nightingale.setIdNum(95366);
+        nightingale.setGpa(3.92);
+        System.out.println("\nFlorence's ID: " + nightingale.getIdNum());
+        System.out.println("Florence's GPA: " + nightingale.getGpa());
+
+        // Call getters to output Grace’s first name and CS status
+        System.out.println("\nFirst name: " + hopper.getFirstName());
+        System.out.println("CS status: " + hopper.getIsCsStudent());
+
+        // Call CreateID() for all three
+        System.out.println("\nAlan's ID" + turing.CreateID());
+        System.out.println("Florence's ID" + nightingale.CreateID());
+        System.out.println("Grace's ID" + hopper.CreateID());
+
+        // Call CSGPA() for all three
+        System.out.println("\nAlan's CS GPA: " + turing.CSGPA());
+        System.out.println("Florence's CS GPA: " + nightingale.CSGPA());
+        System.out.println("Grace's CS GPA: " + hopper.CSGPA());
 
     } // end main
 
-    //math.floor divide by 100
 } // end GoodStudentDriver class
